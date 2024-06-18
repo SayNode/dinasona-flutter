1937d98b522243fc239bb5558fbca86a5fa6d029
MoizesFSantos <moizesferreira66@hotmail.com>
2024-06-18 11:47:10 +0100

    chore: Refactor beneficiary_root_controller.dart

--- lib/pages/root/controllers/beneficiary_root_controller.dart
+++ lib/pages/root/controllers/beneficiary_root_controller.dart
@@ -8,6 +8,7 @@ class BeneficiaryRootController extends GetxController {
   final List<Widget> body =
       BeneficaryItem.values.map((BeneficaryItem e) => e.page).toList();
 
+  // ignore: use_setters_to_change_properties
   void changeTabIndex(int index) {
     tabIndex.value = index;
   }
