9a6c149b9e3843ee1f1c8e4cab35a13a227260d4
ruby1213 <121198995+ruby1213@users.noreply.github.com>
2024-07-09 11:19:45 +0200

    [DPS] DIN-67 Rh donor settings username and password (#26)
    * feat: Update donor personal details page
    
    The donor personal details page has been updated to include separate fields for the first name and second name. This change required modifications to the controller and the personal details page. The first name and second name text fields have been added to the personal details page, and the corresponding text controllers have been updated in the controller.
    
    * feat: Separate first name and last name fields in donor personal details page
    
    * feat: Add country field to User model
    
    * feat: Add location field to donor personal details page
    
    * feat: Fix backend call in donor personal details save function
    
    The backend call in the save function of the DonorPersonalDetailsController has been fixed to update the user's name and country. The first name and second name text fields are used to construct the name, and the location field is used to determine the country. If an error occurs during the update, it is logged in the controller.
    
    * feat: Update donor personal details page
    
    The donor personal details page has been updated to include separate fields for the first name and second name. This change required modifications to the controller and the personal details page. The first name and second name text fields have been added to the personal details page, and the corresponding text controllers have been updated in the controller.

--- lib/pages/donor_settings_page/controllers/donor_personal_details_controller.dart
+++ lib/pages/donor_settings_page/controllers/donor_personal_details_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -7,21 +9,34 @@ class DonorPersonalDetailsController extends GetxController {
   RxBool loading = false.obs;
 
   final UserStateService userStateService = Get.find<UserStateService>();
-  final TextEditingController nameController = TextEditingController();
+  final TextEditingController fistNameController = TextEditingController();
+  final TextEditingController secondNameController = TextEditingController();
   final TextEditingController loactionController = TextEditingController();
+
+  RxString location = ''.tr.obs;
 
   @override
   void onInit() {
     super.onInit();
-    nameController.text = userStateService.user.value.name;
+    final List<String> nameParts = userStateService.user.value.name.split(' ');
+    fistNameController.text = nameParts[0];
+    secondNameController.text = nameParts.length > 1 ? nameParts[1] : '';
   }
 
   Future<void> save() async {
     //TODO: fix the backend call
-    loading.value = true;
-    await userStateService.updateUserInfo(<String, dynamic>{
-      'name': nameController.text,
-    });
-    loading.value = false;
+    try {
+      loading.value = true;
+      await userStateService.updateUserInfo(<String, dynamic>{
+        'name':
+            '${fistNameController.text.trim()} ${secondNameController.text.trim()}',
+        'country': location.value.toLowerCase(),
+      });
+      loading.value = false;
+      Get.back();
+    } catch (e) {
+      loading.value = false;
+      log(e.toString(), name: 'DonorPersonalDetailsController');
+    }
   }
 }
