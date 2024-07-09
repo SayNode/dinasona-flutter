e62260a61e444e2e19e147c2c12a776a4ebd89fa
= <julien33widmer@gmail.com>
2024-07-09 15:49:14 +0200

    Fix donated amount if null

--- lib/model/donor_statistics.dart
+++ lib/model/donor_statistics.dart
@@ -36,11 +36,11 @@ class DonorStatistics {
 
   factory DonorStatistics.fromMap(Map<String, dynamic> map) {
     return DonorStatistics(
-      totalAmountDonated: map['total_amount_donated'] as double? ?? -1,
+      totalAmountDonated: map['total_amount_donated'] as double? ?? 0,
       totalCountriesDonatedTo:
-          map['total_beneficiaries_donated_to'] as int? ?? -1,
+          map['total_beneficiaries_donated_to'] as int? ?? 0,
       totalBeneficiariesDonatedTo:
-          map['total_countries_donated_to'] as int? ?? -1,
+          map['total_countries_donated_to'] as int? ?? 0,
     );
   }
 
