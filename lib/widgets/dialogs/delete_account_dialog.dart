7cfd11b32eac8571d9ed31f7baaa7a84244662e4
Moizes Ferreira dos Santos <77401158+MoizesFSantos@users.noreply.github.com>
2024-07-11 14:40:14 +0000

    DIN-68 [DPS] Donor Settings Logout and Delete Account (#25)
    * feat: Add logout and delete account popups
    
    This commit adds two new popups for logging out and deleting the user account. The `openLogoutPopup` and `openDeleteAccountPopup` methods are added to the `PopupManager` class. These methods open the respective popups using the `openCustomPopup` function.
    
    The `DonorSettingsPageController` is updated to use the new popup methods instead of directly calling the `LogoutDialog` and `DeleteAccountDialog` classes.
    
    The `DeleteAccountDialog` class is also updated to use a `SizedBox` and `Padding` to provide better spacing and layout for the dialog content.
    
    This change improves the user experience by providing dedicated popups for logging out and deleting the user account.
    
    * feat: Update donor settings page with new popups
    
    The donor settings page has been updated to use the new `openLogoutPopup` and `openDeleteAccountPopup` methods from the `PopupManager` class. This change replaces the direct usage of the `LogoutDialog` and `DeleteAccountDialog` classes. Additionally, the `DeleteAccountDialog` class has been improved with better spacing and layout using `SizedBox` and `Padding`.
    
    This update enhances the user experience by providing dedicated popups for logging out and deleting the user account.

--- lib/widgets/dialogs/delete_account_dialog.dart
+++ lib/widgets/dialogs/delete_account_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 
 import '../../pages/settings/widget/delete_account_bottom_sheet.dart';
@@ -14,43 +15,57 @@ class DeleteAccountDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final CustomTheme theme = Get.find<ThemeService>().theme;
-    return AlertDialog(
-      icon: Image.asset(
-        'assets/images/delete_user.png',
-        width: 90,
-        height: 90,
-      ),
-      title: Text(
-        'Delete account'.tr,
-        style: CustomTypography.fromColor(theme.shadowed).k24Bold,
-      ),
-      content: Text(
-        textAlign: TextAlign.center,
-        'Just checking: are you sure you want to delete?'.tr,
-        style: CustomTypography.fromColor(theme.graphite).k16Reg,
-      ),
-      actionsAlignment: MainAxisAlignment.spaceBetween,
-      actions: <Widget>[
-        DinasonaButton(
-          expand: false,
-          color: theme.silvershine,
-          text: 'Yes, I am'.tr,
-          textColor: theme.graphite,
-          onPressed: () {
-            Get.bottomSheet(
-              const DeleteAccountBottomSheet(),
-            );
-          },
-          padding: EdgeInsets.all(getRelativeWidth(14)),
+    return SizedBox(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/delete_user.png',
+              width: 90,
+              height: 90,
+            ),
+            Gap(getRelativeHeight(20)),
+            Text(
+              'Delete account'.tr,
+              style: CustomTypography.fromColor(theme.shadowed).k24Bold,
+            ),
+            Text(
+              textAlign: TextAlign.center,
+              'Just checking: are you sure you want to delete?'.tr,
+              style: CustomTypography.fromColor(theme.graphite).k16Reg,
+            ),
+            Gap(getRelativeHeight(10)),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                DinasonaButton(
+                  expand: false,
+                  color: theme.silvershine,
+                  text: 'Yes, I am'.tr,
+                  textColor: theme.graphite,
+                  onPressed: () {
+                    Get.bottomSheet(
+                      const DeleteAccountBottomSheet(),
+                    );
+                  },
+                  padding: EdgeInsets.all(getRelativeWidth(14)),
+                ),
+                Gap(getRelativeWidth(30)),
+                DinasonaButton(
+                  expand: false,
+                  color: theme.amberglow,
+                  text: 'No, I am not'.tr,
+                  onPressed: Get.back,
+                  padding: EdgeInsets.all(getRelativeWidth(14)),
+                ),
+              ],
+            ),
+          ],
         ),
-        DinasonaButton(
-          expand: false,
-          color: theme.amberglow,
-          text: 'No, I am not'.tr,
-          onPressed: Get.back,
-          padding: EdgeInsets.all(getRelativeWidth(14)),
-        ),
-      ],
+      ),
     );
   }
 }
