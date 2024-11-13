c3abd8ee6b16892e6174fddc741ee19c0000c151
Julien Widmer <julien33widmer@gmail.com>
2024-11-13 17:49:12 +0100

    Fix UserStateService - Donor and Beneficiary get different data format from endpoint (#78)

--- lib/service/user_state_service.dart
+++ lib/service/user_state_service.dart
@@ -172,11 +172,21 @@ class UserStateService extends GetxService {
         ),
       );
       if (response.statusCode == 200) {
-        final Map<String, dynamic> data =
-            // ignore: avoid_dynamic_calls
-            response.data['user'] as Map<String, dynamic>;
+        final Map<String, dynamic> data;
+        if (user.value.isDonor) {
+          data =
+              // ignore: avoid_dynamic_calls
+              response.data['result'] as Map<String, dynamic>;
+        } else {
+          data =
+              // ignore: avoid_dynamic_calls
+              response.data['user'] as Map<String, dynamic>;
+        }
+
         // ignore: no_leading_underscores_for_local_identifiers
-        final User _user = User.fromJson(data);
+        final User _user = User.fromJson(
+          data,
+        );
         // ignore: cascade_invocations
         _user.beneficiary = Beneficiary.anonymous();
         user.value = _user;
