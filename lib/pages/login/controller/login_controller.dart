b5ee6a3bfbbeb70a1c4f6f107bf53aedbb0c1748
ruby1213 <121198995+ruby1213@users.noreply.github.com>
2024-11-14 11:20:47 +0100

    Replace Get.to with Get.offAll in LoginController and SignupController for improved navigation

--- lib/pages/login/controller/login_controller.dart
+++ lib/pages/login/controller/login_controller.dart
@@ -63,7 +63,7 @@ class LoginController extends GetxController {
           password.clear();
           email.clear();
           unawaited(
-            Get.to<void>(
+            Get.offAll<void>(
               () => Get.find<UserStateService>().user.value.isDonor
                   ? const DonorRootPage()
                   : const BeneficiaryRootPage(),
--- lib/pages/sign_up/controller/sign_up_controller.dart
+++ lib/pages/sign_up/controller/sign_up_controller.dart
@@ -109,7 +109,7 @@ class SignupController extends GetxController {
     email.clear();
     showPopup(isBeneficiary: isBeneficiary);
     unawaited(
-      Get.to(
+      Get.offAll(
         () =>
             isBeneficiary ? const BeneficiaryRootPage() : const DonorRootPage(),
       ),
