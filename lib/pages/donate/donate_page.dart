d92cc318944be6a843fe27bbc55c42bd837ae5ff
Curtis Sanderson <55060423+curtis2101@users.noreply.github.com>
2024-11-26 11:35:32 +0100

    DIN-18 fixed donations list (#102)
    * fixed donations list
    
    * added refresh

--- lib/pages/donate/donate_page.dart
+++ lib/pages/donate/donate_page.dart
@@ -17,60 +17,66 @@ class DonatePage extends GetView<DonatePageController> {
   Widget build(BuildContext context) {
     final CustomTheme theme = Get.put(ThemeService()).theme;
     Get.put(DonatePageController());
-    return SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: getRelativeHeight(20)),
-        child: Column(
-          children: <Widget>[
-            Gap(getRelativeHeight(20)),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Donation history'.tr,
-                style: CustomTypography.fromColor(theme.shadowed).k24Bold,
+    return RefreshIndicator(
+      onRefresh: () async {
+        await controller.onInit();
+      },
+      child: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: getRelativeHeight(20)),
+          child: Column(
+            children: <Widget>[
+              Gap(getRelativeHeight(20)),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Donation history'.tr,
+                  style: CustomTypography.fromColor(theme.shadowed).k24Bold,
+                ),
               ),
-            ),
-            Gap(getRelativeHeight(16)),
-            Obx(
-              () => controller.needsFulfilled.isEmpty
-                  ? Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Divider(
-                            color: theme.shadowed,
-                            thickness: 1,
-                            height: 2,
-                          ),
-                        ),
-                        const Gap(16),
-                        Text(
-                          'No donations made yet'.tr,
-                          style:
-                              CustomTypography.fromColor(theme.shadowed).k16Reg,
-                        ),
-                        const Gap(16),
-                        Expanded(
-                          child: Divider(
-                            color: theme.shadowed,
-                            thickness: 1,
-                            height: 2,
-                          ),
-                        ),
-                      ],
-                    )
-                  : Column(
-                      children: <Widget>[
-                        for (final Need need in controller.needsFulfilled)
-                          Padding(
-                            padding: EdgeInsets.only(
-                              bottom: getRelativeHeight(16),
+              Gap(getRelativeHeight(16)),
+              Obx(
+                () => controller.needsFulfilled.isEmpty
+                    ? Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Divider(
+                              color: theme.shadowed,
+                              thickness: 1,
+                              height: 2,
                             ),
-                            child: NeedCard(need: need),
                           ),
-                      ],
-                    ),
-            ),
-          ],
+                          const Gap(16),
+                          Text(
+                            'No donations made yet'.tr,
+                            style: CustomTypography.fromColor(theme.shadowed)
+                                .k16Reg,
+                          ),
+                          const Gap(16),
+                          Expanded(
+                            child: Divider(
+                              color: theme.shadowed,
+                              thickness: 1,
+                              height: 2,
+                            ),
+                          ),
+                        ],
+                      )
+                    : Column(
+                        children: <Widget>[
+                          for (final Need need in controller.needsFulfilled)
+                            Padding(
+                              padding: EdgeInsets.only(
+                                bottom: getRelativeHeight(16),
+                              ),
+                              child: NeedCard(need: need),
+                            ),
+                        ],
+                      ),
+              ),
+            ],
+          ),
         ),
       ),
     );
