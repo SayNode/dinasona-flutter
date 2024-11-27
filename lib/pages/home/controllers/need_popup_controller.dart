e1e4a51a1258d86a5098f65c158aef064cd73789
MoizesFSantos <moizesferreira66@hotmail.com>
2024-11-27 13:36:17 +0000

    Refactor need popup logic to use isLocket method for donation button state and add wallet connection prompt

--- lib/pages/home/controllers/need_popup_controller.dart
+++ lib/pages/home/controllers/need_popup_controller.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import '../../../model/need.dart';
 import '../../../service/api_service.dart';
 import '../../../service/logger_service.dart';
+import '../../../service/wallet_service.dart';
 import '../../wallet/send_bitcoin_page.dart';
 
 class NeedPopupController extends GetxController {
@@ -12,6 +13,16 @@ class NeedPopupController extends GetxController {
   final Need need;
   final APIService apiService = Get.find<APIService>();
   final LoggerService logger = Get.find<LoggerService>();
+
+  bool isLocket() {
+    if (need.status == NeedStatus.past ||
+        Get.find<WalletService>().balanceInUserCurrency < need.amount ||
+        Get.find<WalletService>().isWalletConnected.value == false) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   Future<void> donate(Need need) async {
     unawaited(
