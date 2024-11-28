76fc7063d8ba7b4005dbca0725cd36e4a2c96704
Curtis Sanderson <55060423+curtis2101@users.noreply.github.com>
2024-11-28 10:45:15 +0100

    added to 3

--- lib/pages/wallet/widgets/wallet_info_card.dart
+++ lib/pages/wallet/widgets/wallet_info_card.dart
@@ -55,7 +55,7 @@ class WalletInfoCard extends GetView<WalletPageController> {
               Gap(getRelativeHeight(5)),
               Obx(
                 () => Text(
-                  '${localizationController.selectedCurrency['sign']} ${walletService.balanceInUserCurrency.value}',
+                  '${localizationController.selectedCurrency['sign']} ${walletService.balanceInUserCurrency.value.toStringAsFixed(3)}',
                   style: CustomTypography.fromColor(theme.moonstone).k36Bold,
                 ),
               ),
