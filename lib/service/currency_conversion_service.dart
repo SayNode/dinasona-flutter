76db06b6e30c250ad655ec109911f6f35b9895f2
= <julien33widmer@gmail.com>
2025-01-22 12:52:47 +0100

    Remove print

--- lib/service/currency_conversion_service.dart
+++ lib/service/currency_conversion_service.dart
@@ -83,9 +83,6 @@ class CurrencyConversionService extends GetxService {
 
   Future<double> fetchConversionRateBTCUserCurrency() async {
     http.Response response = http.Response('', 999);
-    print(
-      'test3 ${localizationController.selectedCurrency.value.code.toLowerCase()}',
-    );
     try {
       if (!currencyConversionRateDebouncing) {
         currencyConversionRateDebouncing = true;
