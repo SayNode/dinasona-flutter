c47416aba32808f242afa89cc33354ea8e9e56b0
Julien Widmer <julien33widmer@gmail.com>
2025-02-19 15:23:26 +0100

    DIN-266 - Beneficiary: need description (#148)
    * Make textfield responsive and add textfield scroll
    
    * Fix needcards don't show all the information
    
    * Fix overflow issues
    
    * Fix min height for alternate need cards
    
    * Nothing to see here
    
    * Remove overflow case
    
    ---------
    
    Co-authored-by: Moizes Ferreira dos Santos <77401158+MoizesFSantos@users.noreply.github.com>

--- lib/pages/create_new_need/need_screen4.dart
+++ lib/pages/create_new_need/need_screen4.dart
@@ -6,6 +6,7 @@ import '../../service/theme_service.dart';
 import '../../theme/theme.dart';
 import '../../theme/typography.dart';
 import '../../util/util.dart';
+import '../../widgets/custom_scaffold.dart';
 import '../../widgets/dinasona_button.dart';
 import 'controller/create_new_need_controller.dart';
 
@@ -15,59 +16,66 @@ class NeedScreen4 extends GetView<CreateNewNeedController> {
   @override
   Widget build(BuildContext context) {
     final CustomTheme theme = Get.put(ThemeService()).theme;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Gap(getRelativeHeight(40)),
-        Text(
-          'Tell your story: describe your need'.tr,
-          style: CustomTypography.fromColor(theme.shadowed).k16SemiBold,
-        ),
-        Gap(getRelativeHeight(20)),
-        Expanded(
-          flex: 3,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                color: theme.shadowed.withAlpha(128),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                textInputAction: TextInputAction.done,
-                controller: controller.screen4,
-                minLines: 20,
-                maxLines: 20,
-                maxLength: controller.descriptionMaxLenth,
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Tell your story: describe your need',
-                ),
+    return CustomScaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Tell your story: describe your need'.tr,
+            style: CustomTypography.fromColor(theme.shadowed).k16SemiBold,
+          ),
+          Gap(getRelativeHeight(5)),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: theme.shadowed.withAlpha(128),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: TextField(
+                        textInputAction: TextInputAction.newline,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        controller: controller.screen4,
+                        maxLength: controller.descriptionMaxLenth,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Tell your story: describe your need',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-        ),
-        const Spacer(),
-        Row(
-          children: <Widget>[
-            DinasonaButton(
-              expand: false,
-              text: 'Back'.tr,
-              onPressed: () => controller.selectTab(NeedsTab.screen3),
-            ),
-            const Gap(10),
-            Expanded(
-              child: DinasonaButton(
-                text: 'Continue'.tr,
-                onPressed: () => controller.selectTab(NeedsTab.screen5),
+          const Spacer(),
+          Row(
+            children: <Widget>[
+              DinasonaButton(
+                expand: false,
+                text: 'Back'.tr,
+                onPressed: () => controller.selectTab(NeedsTab.screen3),
               ),
-            ),
-          ],
-        ),
-        Gap(getRelativeHeight(20)),
-      ],
+              const Gap(10),
+              Expanded(
+                child: DinasonaButton(
+                  text: 'Continue'.tr,
+                  onPressed: () => controller.selectTab(NeedsTab.screen5),
+                ),
+              ),
+            ],
+          ),
+          Gap(getRelativeHeight(20)),
+        ],
+      ),
     );
   }
 }
