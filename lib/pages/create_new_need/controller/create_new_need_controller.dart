22bcf902db9803094d62cdeeee03a87c2ef776d6
Moizes Ferreira dos Santos <77401158+MoizesFSantos@users.noreply.github.com>
2025-02-24 13:47:55 +0000

    DIN-269 [DG] Beneficiary: Edit need category  (#153)
    * Update API_URL for development environment and enhance area selection in CreateNewNeedController
    
    * Add area of interest to need update functionality
    
    ---------
    
    Co-authored-by: Julien Widmer <julien33widmer@gmail.com>

--- lib/pages/create_new_need/controller/create_new_need_controller.dart
+++ lib/pages/create_new_need/controller/create_new_need_controller.dart
@@ -81,6 +81,10 @@ class CreateNewNeedController extends GetxController {
           : need!.amount.toString();
 
       screen1.text = need!.title;
+      for (int i = 0; i < need!.areasOfInterest.length; i++) {
+        selectedAreasOfInterest.add(need!.areasOfInterest[i]);
+      }
+
       screen3.text = (double.parse(tmpAmount) /
               currencyConversionService.conversionRates.value.USRvsUSD)
           .toStringAsFixed(2);
@@ -275,6 +279,13 @@ class CreateNewNeedController extends GetxController {
   }
 
   Future<void> updateNeed(int id, bool isDraft) async {
+    final String areasOfInterest = selectedAreasOfInterest
+        .map(
+          (AreaOfInterest e) => e.title,
+        )
+        .toString()
+        .replaceAll('(', '')
+        .replaceAll(')', '');
     final double needAmountInUSD = double.parse(screen3.text) *
         currencyConversionService.conversionRates.value.USRvsUSD;
 
@@ -292,6 +303,7 @@ class CreateNewNeedController extends GetxController {
       'title': screen1.text,
       'description': screen4.text,
       'amount': needAmountInUSD.toStringAsFixed(2),
+      'area_of_interest': areasOfInterest,
       'bolt11Invoice': bolt11Invoice,
       'status': isDraft ? 'draft' : 'published',
     });
