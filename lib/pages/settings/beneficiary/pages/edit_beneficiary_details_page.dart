5b989db2e82f872f38de07c8998b1814dc214034
ruby1213 <121198995+ruby1213@users.noreply.github.com>
2025-02-26 16:49:03 +0100

    Reduce maximum length for beneficiary name field from 50 to 40 characters

--- lib/pages/settings/beneficiary/pages/edit_beneficiary_details_page.dart
+++ lib/pages/settings/beneficiary/pages/edit_beneficiary_details_page.dart
@@ -67,7 +67,7 @@ class EditBeneficiaryPage
                         CustomTypography.fromColor(theme.shadowed).k16Reg,
                     hintStyle:
                         CustomTypography.fromColor(theme.graphite).k16Reg,
-                    maxLength: 50,
+                    maxLength: 40,
                   ),
                   const Gap(6),
                   Row(
